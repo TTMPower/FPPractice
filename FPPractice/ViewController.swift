017bd797ebd3ecda9c32aeec17e956fb912a8d23
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-06-29 12:07:51 +0600

    Initial Commit

---
+++ FPPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FPPractice
+//
+//  Created by Владислав Вишняков on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
